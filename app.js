0480ab9943f1b60cde08a08c2d093157067a5511
David Astudillo <davidadbeelgonzalez@gmail.com>
2022-07-18 13:02:04 -0400

    adding handlebars and dotenv

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
+//imports
 import express from "express";
+import hbs from "hbs";
+import * as dotenv from 'dotenv'
 
+//dotenv
+dotenv.config()
+
+//dirname and filename
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+//Init vars express
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
-app.set('view engine', 'hbs');
+//Handlebars
+hbs.registerPartials(`${__dirname}/views/components`);
+app.set("view engine", "hbs");
 
+//Express
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-    res.render(`home`, {name: 'David Astudillo', title:'Node course'});
+    res.render(`home`, { name: "David Astudillo", title: "Node course" });
 });
 
 app.get("/elements", (req, res) => {
-    res.sendFile(`${__dirname}/public/elements.html`);
+    res.render(`elements`, { name: "David Astudillo", title: "Node course" });
 });
 
 app.get("/generic", (req, res) => {
-    res.sendFile(`${__dirname}/public/generic.html`);
+    res.render(`generic`, { name: "David Astudillo", title: "Node course" });
 });
+
 app.get("*", (req, res) => {
-    res.sendFile(`${__dirname}/public/404.html`);
+    res.render(`404`);
 });
 
 app.listen(port, () => console.log(`Listening in the ${port} port`));
